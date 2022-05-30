cf3d4cf3eeb578a9dd282e2a6ee7e0b3cd1c445c
Rox-Quode <92692755+Rox-Quode@users.noreply.github.com>
2022-05-31 00:14:57 +0300

    created light theme

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
 });
+
+AOS.init({  
+offset: 150, // offset (in px) from the original trigger point
+delay: 100, // values from 0 to 3000, with step 50ms
+duration: 1500, // values from 0 to 3000, with step 50ms
+easing: 'ease', // default easing for AOS animations
+once: false
+});
