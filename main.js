6bf1e69425e9cbf13a414a1bed587c3e65ffaf85
Rox-Quode <92692755+Rox-Quode@users.noreply.github.com>
2022-05-31 17:18:02 +0300

    created color palettes for dark mode

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ duration: 1500, // values from 0 to 3000, with step 50ms
 easing: 'ease', // default easing for AOS animations
 once: false
 });
+
+$('.toggle-container').click(function(){
+  
+    $('body').toggleClass('dark-mode-active');
+    $(this).toggleClass('dark-mode-active');
+    
+  });
+  
